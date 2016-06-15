1693bc765f96930ad44d77f3f77862047ffbf239
Chuck D'Antonio <c_dantonio@acm.org>
2016-06-15 14:45:15 -0700

    Updated message

--- src/main/java/pcf/immersion/HelloController.java
+++ src/main/java/pcf/immersion/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Why hello there. Welcome to Spring Boot! ";
     }
 
 }
